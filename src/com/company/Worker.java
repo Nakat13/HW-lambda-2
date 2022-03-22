4b24a14bfd3af4de8604776b8e09973472adab5c
nakat13 <denisovaekaterina13@ya.ru>
2022-03-22 09:15:41 +0300

    Version 2 - added functional interfaces

--- src/com/company/Worker.java
+++ src/com/company/Worker.java
@@ -1,15 +1,6 @@
 package com.company;
 
 public class Worker {
-    @FunctionalInterface
-    public interface OnTaskDoneListener {
-        void onDone(String result);
-    }
-
-    @FunctionalInterface
-    public interface OnTaskErrorListener {
-        void onError(String result);
-    }
 
     private OnTaskDoneListener callback;
     private OnTaskErrorListener errorCallback;
